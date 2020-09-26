f9c5b9d60d3e9afd2dcb7af21880ed23d766306f
MooseTheGoose <moosa.7@buckeyemail.osu.edu>
2020-09-26 14:44:47 -0400

    Incorporate dependencies via vcpkg and MSBuild

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # puppet
-DSL with similar syntax to Javascript for use with WebDriver
+DSL with similar syntax to Javascript for use with WebDriver.
+
+# External Dependencies
+Puppet has the following libraries as external dependencies.
+
+* MPIR
+* libcurl
+
+vcpkg and MSBuild are used to resolve these at the moment.
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,30 @@
-#include <string>
 #include "puppet_types.hpp"
-#include "basic_webdrivers.hpp"
+#include <gmp.h>
+#include <curl/curl.h>
+
+size_t write_response(void *ptr, size_t sz, size_t nmemb, string *s) {
+  s->append((char *)ptr, sz * nmemb);
+  return sz * nmemb;
+}
 
 int main(int argc, const char *argv[]) {
-  BasicHeaders hdrs = BasicHeaders();
-  BasicWebdriver webdrvr = BasicWebdriver();
-  int status;
-  WSADATA wsaData;
+  CURL *curl;
+  CURLcode res;
+  mpz_t n;
 
-  string bodystr = "{ \"capabilities\" : { \"alwaysMatch\" : { \"browserName\" : \"firefox\"} } }";
-  vector<char> body = vector<char>();
+  mpz_init(n);
 
-  for(int i = 0; i < bodystr.size(); i++) {
-    body.push_back(bodystr[i]);
-  }
+  string s = "";
 
-  status = WSAStartup(MAKEWORD(2,2), &wsaData);
-  if(status) {
-    fprintf(stderr, "WSAStartup failed...\r\n");
-    return -1;
-  } 
 
-  hdrs.ManipulateHeader("Content-Length", std::to_string(body.size()));
-  vector<char> data = webdrvr.fetch("POST", "/session", hdrs, body);
-  printf("Connection success\r\n%s\r\n", data.data());
+  curl = curl_easy_init();
+  curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:4444/status");
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_response);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+  res = curl_easy_perform(curl);
+
+  printf("%s\r\n", s.data());
+  curl_easy_cleanup(curl);
+
   return 0;
 }
